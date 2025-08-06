db8f4312381380fab63fe332d807215dc22edaa0
Jose Munoz <jmunoz@griddynamics.com>
2025-08-06 12:05:27 -0600

    feat: Add models and sufggest sample products and categories

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,17 @@
+# marketplace/admin.py
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "parent", "created_at", "modified_at")
+    search_fields = ("name",)
+    list_filter = ("created_at", "modified_at")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category", "created_at", "modified_at")
+    search_fields = ("name", "description")
+    list_filter = ("category", "created_at", "modified_at")
